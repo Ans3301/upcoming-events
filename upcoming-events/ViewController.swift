a6865f707293a1304b0c345fe4300394b3bfb97c
Anisovich_Maria <anisovich.masha@gmail.com>
2025-03-10 12:30:54 +0100

    Initial Commit

---
+++ upcoming-events/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  upcoming-events
+//
+//  Created by Мария Анисович on 10.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
